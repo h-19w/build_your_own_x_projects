8c0425f441f327a094e643fca1aacee8278b99f6
h-19w <167726843+h-19w@users.noreply.github.com>
2026-05-12 12:37:46 +0800

    ye

--- python search engine/readme.md
+++
@@ -1,7 +0,0 @@
-<!-- https://youtu.be/cY7pE7vX6MU?si=wcS7pgCiNjYo88j7 -->
-
-indexing 
-searching 
-faceting 
-more like this 
-main 
--- website/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-views = Blueprint("views", __name__)
-# from . import routes --- IGNORE ---
-
-@views.route("/") # decorator to specify the URL for this view function
-def home():
-    return "<h1>Welcome to the Home Page!</h1>" # return a simple string as the response
